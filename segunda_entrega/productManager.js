0fb27d721f5b8995bdcb8e60c7ee62bf845755a2
Petit Martin Cesar Gaston <cpetit@inter.corp>
2023-09-25 10:40:52 -0300

    subida entrega 2

--- segunda_entrega/productManager.js
+++ segunda_entrega/productManager.js
@@ -19,8 +19,6 @@ class ProductsManager {
   }
 
   async createProduct(newProduct) {
-    console.log("producto", newProduct);
-
     const { title, description, price, thumbnail, code, stock } = newProduct;
 
     if (
@@ -35,7 +33,7 @@ class ProductsManager {
     }
 
     const products = await getJSONFromFile(this.path);
-    const _id = products.length + 1;
+    const _id = (await this.getUltimoId()) + 1;
     newProduct = { ...newProduct, id: _id };
     products.push(newProduct);
     await saveJSONToFile(this.path, products);
@@ -45,14 +43,50 @@ class ProductsManager {
     return getJSONFromFile(this.path);
   }
 
-  async getProdutById(id) {
+  async getProductById(id) {
     try {
-      const productos = getJSONFromFile(this.path);
+      const productos = await getJSONFromFile(this.path);
       const producto = productos.find((p) => p.id === id);
       if (!producto) throw new Error(`id Inexistente`);
+      return producto;
     } catch (error) {
       throw new Error(`No de Pudo Procesar la Solicitud ${error} `);
     }
+  }
+
+  async updateProductById(id, productoActualizado) {
+    const productos = await getJSONFromFile(this.path);
+    let producto = productos.find((p) => p.id === id);
+    producto = { ...productoActualizado, id: producto.id };
+
+    const index = productos.findIndex((p) => p.id === id);
+    if (index !== -1) {
+      productos[index] = producto;
+    }
+    await saveJSONToFile(this.path, productos);
+    return producto;
+  }
+
+  async deleteProductByid(id) {
+    try {
+      const productos = await getJSONFromFile(this.path);
+      let producto = productos.find((p) => p.id === id);
+      let nuevaLista = productos.filter((p) => p.id !== id);
+      await saveJSONToFile(this.path, nuevaLista);
+      return producto;
+    } catch (error) {
+      throw Error("fallo la eliminacion", error);
+    }
+  }
+
+  async getUltimoId() {
+    const productos = await getJSONFromFile(this.path);
+   
+    if (productos.length === 0) return 0;
+
+    const ultimoRegistro = productos.length - 1;
+    const ultimoId = productos[ultimoRegistro].id;
+    return ultimoId;
   }
 }
 
@@ -79,7 +113,7 @@ const saveJSONToFile = async (path, data) => {
   }
 };
 
-const testPrueba = async () => {
+const testCrear = async () => {
   const path = "./products.json";
 
   try {
@@ -87,17 +121,17 @@ const testPrueba = async () => {
 
     const productManager = new ProductsManager(path);
     const prroductoNuevo = new Product(
-      "pera",
-      "naranja Deliciosa",
+      "Naranja",
+      "Naranja Rica",
       2500,
       "sin imagen",
-      "abc123",
+      "abc124",
       25
     );
 
     // testing add products
-    productManager.createProduct(prroductoNuevo);
-    const products = await productManager.get();
+
+    await productManager.createProduct(prroductoNuevo);
 
     // Traigo todos los productos Archivo
 
@@ -105,18 +139,100 @@ const testPrueba = async () => {
     console.log("productos del Archivo");
     console.log("---------------------");
     console.log(productosArchivos);
-
-    // Traigo el Producto Por ID
-
-    const id = 1;
-
-    const producto = await productManager.getProdutById(id);
-    console.log(`Producto con el id ${id}`);
-    console.log("---------------------");
-    console.log(productosArchivos);
   } catch (error) {
     console.error("Ha ocurrido un error Al generar Poducto", error.message);
   }
 };
 
-testPrueba();
+const testBuscar = async () => {
+  const path = "./products.json";
+  const productManager = new ProductsManager(path);
+
+  try {
+    // Traigo el Producto Por ID
+
+    const id = 2;
+    const producto = await productManager.getProductById(id);
+    console.log(`Producto con el id ${id}`);
+    console.log("---------------------");
+    console.log(producto);
+  } catch (error) {
+    throw Error("Error al buscar producto: ", error);
+  }
+};
+
+const testActualizar = async () => {
+  const path = "./products.json";
+  const productManager = new ProductsManager(path);
+
+  try {
+    const id = 4;
+
+    // producto A actualizar
+    const productoModificado = new Product(
+      "Morron",
+      "Morron Rojo",
+      4800,
+      "sin imagen Nueva",
+      "abc128",
+      25
+    );
+
+    // leo el producto original
+
+    const productoOriginal = await productManager.getProdutById(id);
+
+    // Actualizo el producto
+
+    const productoActualizado = await productManager.updateProductById(
+      id,
+      productoModificado
+    );
+    console.log("producto Original");
+    console.log("-----------------");
+    console.log(productoOriginal);
+
+    console.log("producto Actualizado");
+    console.log("-----------------");
+    console.log(productoActualizado);
+  } catch (error) {
+    throw Error("Error al actualizar producto: ", error);
+  }
+};
+
+const testEliminar = async () => {
+  const path = "./products.json";
+  const productManager = new ProductsManager(path);
+
+  try {
+    const id = 1;
+
+    // leo el producto original
+
+    const productoEliminado = await productManager.deleteProductByid(id);
+    console.log(
+      `El producto ${productoEliminado.title} fue eliminado correctamente`
+    );
+  } catch (error) {
+    throw Error("Error al Eliminar producto: ", error);
+  }
+};
+
+const testMostrar = async () => {
+  const path = "./products.json";
+  const productManager = new ProductsManager(path);
+  try {
+    const productos = await productManager.get();
+    console.log("LISTADO PRODUCTOS FINAL");
+    console.log("-----------------------");
+    console.log(productos);
+  } catch (error) {
+    throw Error("Error al mostrar producto: ", error);
+  }
+};
+
+testCrear();
+testBuscar();
+testActualizar();
+testEliminar();
+testMostrar();
