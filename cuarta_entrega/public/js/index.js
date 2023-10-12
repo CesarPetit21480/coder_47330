937d04e78ccc8ee86805eb421f7a9f6e3b1300f6
Petit Martin Cesar Gaston <cpetit@inter.corp>
2023-10-12 10:06:12 -0300

    prueba chat

--- cuarta_entrega/public/js/index.js
+++ cuarta_entrega/public/js/index.js
@@ -3,9 +3,9 @@
 //     const formMessage = document.getElementById('form-message');
 //     const inputMessage = document.getElementById('input-message');
 //     const showMessage = document.getElementById('show-message');
-  
+
 //     const socket = io();
-  
+
 //     formMessage.addEventListener('submit', (event) => {
 //       event.preventDefault();
 //       messages.push({
@@ -17,7 +17,7 @@
 //       inputMessage.focus();
 //       updateMessages(messages);
 //     });
-  
+
 //     function updateMessages(messages = []) {
 //       showMessage.innerText = '';
 //       messages.forEach((data) => {
@@ -26,19 +26,68 @@
 //         showMessage.appendChild(item);
 //       })
 //     }
-  
+
 //     socket.on('connect', () => {
 //       console.log('Conectados al servidor');
 //     });
-  
+
 //     socket.on('start', (data) => {
 //       messages = data;
 //       updateMessages(messages);
 //     });
-  
+
 //     socket.on('notification', (data) => {
 //       messages.push(data);
 //       updateMessages(messages);
 //     });
-  
+
 //   })();
+(function () {
+
+const imputMessage = document.getElementById("inputMessage");
+const formChat = document.getElementById("formChat");
+const container_messages = document.getElementById("container_messages");
+const socket = io();
+let messages = [];
+
+formChat.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  messages.push({
+    socketId: socket.id,
+    message: imputMessage.value,
+  });
+  socket.emit('new-message', imputMessage.value);
+  inputMessage.value = '';
+  inputMessage.focus();
+  updateMessages(messages);
+});
+
+const updateMessages = (message=[]) => {
+  container_messages.innerText = "";
+  message.forEach((msg) => {
+    const elemento = document.createElement("li");
+    elemento.innerText = `socket_id : ${msg.socketId} message: ${msg.message}`;
+    container_messages.appendChild(elemento);
+  });
+};
+
+socket.on('connect', () => {
+    console.log('Conectados al servidor');
+  });
+
+  socket.on('start', (data) => {
+    messages = data;
+    updateMessages(messages);
+  });
+
+  socket.on('notification', (data) => {
+    messages.push(data);
+    updateMessages(messages);
+  });
+
+
+})();
+
+
+
--- cuarta_entrega/src/server.js
+++ cuarta_entrega/src/server.js
@@ -5,21 +5,28 @@ import { Server } from "socket.io";
 const server = http.createServer(app);
 const socketServer = new Server(server);
 const port = 8080;
+const messages = [
+  {
+    socketId: "1234",
+    message: "Coder House",
+  },
+];
 
-socketServer.on("connection", (clienteSocket) => {
+
+socketServer.on('connection', (clienteSocket) => {
   console.log(`Nuevo cliente conectado 🎉 (${clienteSocket.id}).`);
- // clienteSocket.emit("start", messages);
+  clienteSocket.emit('start', messages);
 
-  // clienteSocket.on('new-message', (data) => {
-  //   messages.push({ socketId: clienteSocket.id, message: data });
-  //   clienteSocket.broadcast.emit('notification', { socketId: clienteSocket.id, message: data });
-  // });
+  clienteSocket.on('new-message', (data) => {
+    messages.push({ socketId: clienteSocket.id, message: data });
+    clienteSocket.broadcast.emit('notification', { socketId: clienteSocket.id, message: data });
+  });
 
-  clienteSocket.on("disconnect", () => {
+  clienteSocket.on('disconnect', () => {
     console.log(`Cliente desconectado (${clienteSocket.id}) 😨.`);
   });
 });
 
 server.listen(port, () => {
-  console.log(` escuchando http://localhost:${port}`);
+  console.log(`listening http://localhost:${port}`);
 });
