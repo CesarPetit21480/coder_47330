afe9ab88e46a9d33e2c551ff10b93e83bad0ac31
Petit Martin Cesar Gaston <cpetit@inter.corp>
2023-10-18 13:58:56 -0300

    refactor

--- cuarta_entrega/public/js/realTimeProducts.js
+++ cuarta_entrega/public/js/realTimeProducts.js
@@ -9,25 +9,24 @@ const generarCard = (productos) => {
 
     cardProducts.innerHTML = '';
     productos.forEach(product => {
-        const div = document.createElement('div');
-        div.classList.add('cardStyle');
+        const div = document.createElement('span');       
         div.innerHTML = `   
-        <div class="card text-center text-white bg-dark bg-gradient cardStyle">
-        <div class="card-header">
-       ${product.id}
+        <div class="card text-center text-white bg-dark bg-gradient cardStyle ">
+        <div class="card-header me-2">
+          ${product.id}
         </div>
         <div class="card-body">
-        <h5 class="card-title">Stock ${product.title}</h5>
-        <h5 class="card-title">Stock ${product.stock}</h5>
-        <h5 class="card-title"> Price ${product.price}</h5>
-        <p class="card-text text-warning">${product.description}</p>  
+          <h5 class="card-title">Stock ${product.title}</h5>
+          <h5 class="card-title">Stock ${product.stock}</h5>
+          <h5 class="card-title"> Price ${product.price}</h5>
+          <p class="card-text text-warning">${product.description}</p>
         </div>
-        <div class="card-footer  text-info">
-        ${product.category}
+        <div class="card-footer text-info">
+          ${product.category}
         </div>
-        </div>
+      </div>
         `
-        cardProducts.appendChild(div);
+        cardProducts.append(div);
     });
 
 
