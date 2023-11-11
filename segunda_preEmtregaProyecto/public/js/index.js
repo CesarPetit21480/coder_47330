1b88233b1d597caf6f91ba5aa21eaadd5d925214
CesarPetit21480 <cesar_petit@hotmail.com>
2023-11-11 19:26:10 -0300

    changes

--- segunda_preEmtregaProyecto/public/js/index.js
+++ segunda_preEmtregaProyecto/public/js/index.js
@@ -1,29 +1,12 @@
-// const formMessage = document.getElementById('form-message');
-
-
-
-// formMessage.addEventListener('submit', (event) => {
-//     alert('hola');
-//     event.preventDefault();  
-//     inputMessage.value = '';
-//     inputMessage.focus();
-
-//     Swal.fire({
-//         text: "Mensaje Agregado 😊",
-//         toast: true,
-//         position: 'top-right'
-//     });
-// });
-
 
 function handleEnviarClick(event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
   // Obtiene el "id" del botón clicado
   var id = this.getAttribute("data-id");
   const cantidad = document.querySelector('input[name="cantidad"]').value;
 
-  
+
   // Crear un objeto con los datos a enviar
   const data = {
     productId: id,
@@ -63,57 +46,6 @@ var envioButtons = document.querySelectorAll(".envio-button");
 for (var i = 0; i < envioButtons.length; i++) {
   envioButtons[i].addEventListener("click", handleEnviarClick);
 }
-
-
-
-
-
-// document.getElementById("enviarProducto").addEventListener("click", function (event) {
-//   event.preventDefault();
-
-
-
-//   // Obtener los valores de los campos de entrada
-//   const id = document.querySelector('input[name="productId"]').value;
-//   const cantidad = document.querySelector('input[name="cantidad"]').value;
-//   const idProducto = document.querySelector('input[name="idProduct"]').value;
-
-
-
-//   // Crear un objeto con los datos a enviar
-//   const data = {
-//     productId: id,
-//     cantidad: cantidad
-//   };
-
-//   // Realizar la solicitud POST utilizando fetch
-//   fetch('api/cart/manejador', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(data)
-//   })
-//     .then(response => {
-//       if (response.ok) {
-
-//         Swal.fire({
-//           text: "Producto al carrito Agregado 😊",
-//           toast: true,
-//           position: 'top-right'
-//         });
-//         setTimeout(function () {
-//           location.reload(); // Recargar la página
-//         }, 3000);
-//       } else {
-//         console.error("Error al enviar el mensaje");
-//       }
-//     })
-//     .catch(error => {
-//       console.error("Error en la solicitud:", error);
-//     });
-// });
-
 
 document.getElementById("enviar").addEventListener("click", function (event) {
   event.preventDefault();
